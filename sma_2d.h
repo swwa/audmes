0f2172137507c9aefd0d66502af5d00947249781
Johannes Linkels <jlinkels@linkels.net>
2023-06-21 19:58:34 +0000

    Cleaned up commenting. Corrected formatting.

--- sma_2d.h
+++ sma_2d.h
@@ -1,15 +1,9 @@
 /////////////////////////////////////////////////////////////////////////////
 // Name:        sma-2d
 // Purpose:		Simple Moving average over N values for R rows
-// Author:		Johannes Linkels
-// Modified by:
-// Created:     2023-05-29
-// RCS-ID:
-// Copyright:
-// License:
 /////////////////////////////////////////////////////////////////////////////
 /*
- * Copyright (C) 2007-2008 Vaclav Peroutka <vaclavpe@seznam.cz>
+ * Copyright (C) 2023 Johannes Linkels <jlinkels@linxtech.net>
  *
  * Licensed under the GNU General Public License Version 2
  *
@@ -34,76 +28,44 @@
 #pragma interface "sma_2d.h"
 #endif
 
-/*!
- * Includes
- */
-
 #include <iostream>
 #include <vector>
 
-////@begin includes
-////@end includes
-
-/*!
- * Forward declarations
- */
-
-////@begin forward declarations
-////@end forward declarations
-
-/*!
- * Control identifiers
- */
-
-////@begin control identifiers
-////@end control identifiers
-
-/*!
- * Compatibility
- */
-
-/*!
- * SMA_2D class declaration
- */
-
 class SMA_2D {
-
  public:
   /// Constructors
   SMA_2D(int numRecords, int numAverage);
-  ~SMA_2D ();
+  ~SMA_2D();
 
   /// (Re)Initializes the data values
   void Init(int numRecords, int numAverage);
 
   /// Adds a new value to the average at record recordNum
-  int AddVal (int recordNum, double val);
+  int AddVal(int recordNum, double val);
 
   /// Gets the current moving average value at record recordNum
-  double GetSMA (int recordNum);
+  double GetSMA(int recordNum);
 
   /// Get the number of values in the moving average
-  int GetNumAverage ();
+  int GetNumAverage();
 
   /// Get the number of values summed;
-  int GetNumSummed (int recordNum);
+  int GetNumSummed(int recordNum);
 
   /// Set the length of the moving average
-  void SetNumAverage (int numAverage);
+  void SetNumAverage(int numAverage);
 
   /// Set the number of values summed;
-  void SetNumRecords (int numRecords);
+  void SetNumRecords(int numRecords);
 
-private:
-
-  int m_NumRecords;						// number of records (samples)
-  int m_NumAverage;						// number of values used for the average
-  std::vector<int> index;					// index points to latest value in each record
-  std::vector<int> numSummed;				// number of values summed, avg = sum/numSummed
-  std::vector<double> sum;					// current sum of values for each record
-  std::vector<std::vector<double>> prevValues;	// the actual 2D store
+ private:
+  int m_NumRecords;                             // number of records (samples)
+  int m_NumAverage;                             // number of values used for the average
+  std::vector<int> index;                       // index points to latest value in each record
+  std::vector<int> numSummed;                   // number of values summed, avg = sum/numSummed
+  std::vector<double> sum;                      // current sum of values for each record
+  std::vector<std::vector<double>> prevValues;  // the actual 2D store
 };
 
-#endif 
+#endif
 // _SMA_2D
-  
